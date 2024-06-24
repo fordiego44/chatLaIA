6cda78bd4d4e18c218f4e60e5a216e4fd3dc0b1a
Diego Armando <dcondori@rivercon.com>
2024-06-24 21:26:28 +0000

    thrid commit

--- chat/webapp/controller/Main.controller.js
+++ chat/webapp/controller/Main.controller.js
@@ -7,12 +7,15 @@ sap.ui.define([
 
     return Controller.extend("rivercon.chat.controller.Main", {
         onInit: function () {
+
+            this._isPlaying = false;
+
             var html1 = new HTML("html1", {
                 content:
     
                 "<video style='width: 100%; height: 100%; object-fit: cover;' autoplay>" +
                 "<source src='http://www.w3schools.com/html/movie.mp4' type='video/mp4'>" + 
-                "Your browser does not support the video tag." +
+                "Your browser does not support the video tag." + 
                 "</video>"  
                 
             });
@@ -39,12 +42,15 @@ sap.ui.define([
             oView.setModel(oJSONModelMessage, "jsonMessage");
  
             var oJSONModelVisibility = new sap.ui.model.json.JSONModel({
-                visibility: false  
-            });
+                visibility: false,
+                visibilityHeadset: true
+            }); 
+
             oView.setModel(oJSONModelVisibility, "onContainer3");
  
             this._addDelegateToVBox();
-
+            this._addToggleToVBox();
+            this._closeToVBox();
            
 
             // var oPanel = new Panel({
@@ -52,19 +58,50 @@ sap.ui.define([
             // });
 
             this.byId("contenidoM").addItem(html2);
+            
 
+            this._startListeningAnimation();
         }, 
+
+        _startListeningAnimation: function () {
+            var aCircles = [
+                this.byId("circle1"),
+                this.byId("circle2"),
+                this.byId("circle3"),
+                this.byId("circle4")
+            ];
+
+            aCircles.forEach(function (oCircle, index) {
+                // Agrega un pequeño retraso para cada círculo para hacer la animación más interesante
+                setTimeout(function () {
+                    oCircle.addStyleClass("listening");
+                }, index * 1500); // Retraso de 200 ms entre cada círculo
+            });
+        },
+        _stopListeningAnimation: function () {
+            var aCircles = [
+                this.byId("circle1"),
+                this.byId("circle2"),
+                this.byId("circle3"),
+                this.byId("circle4")
+            ];
+
+            aCircles.forEach(function (oCircle) {
+                oCircle.removeStyleClass("listening");
+            });
+        },
      
         openHeadset: function () {
             
-            var oModel = this.getView().getModel("onContainer3");
-            oModel.setProperty("/visibility", true);
-        },
-        closeHeadset: function () {
+            // Obtiene el ícono dentro del VBox
+            var oIcon = this.byId("toggleIcon");
+            this._isPlaying = true;
+            oIcon.setSrc("sap-icon://media-pause"); 
             
             var oModel = this.getView().getModel("onContainer3");
-            oModel.setProperty("/visibility", false);
-        },
+            oModel.setProperty("/visibility", true); 
+            oModel.setProperty("/visibilityHeadset", true); 
+        }, 
         sendHeadset: function () {
             
             var oModel = this.getView().getModel("onContainer3");
@@ -72,14 +109,6 @@ sap.ui.define([
 
             var message = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here, making it look like readable English. ";
             this.addMessage(message);
-        },
-        _addDelegateToVBox: function () {
-            var oVBox = this.byId("myVBox");
-            if (oVBox) {
-                oVBox.addEventDelegate({
-                    onclick: this.sendHeadset.bind(this)
-                });
-            }
         }, 
         onSubmitMessage: function (oEvent) {
             var sMessage = oEvent.getParameter("value");
@@ -125,6 +154,54 @@ sap.ui.define([
                 }, 0);
                 
             }
+        },
+        _addDelegateToVBox: function () {
+            var oVBox = this.byId("myVBox");
+            if (oVBox) {
+                oVBox.addEventDelegate({
+                    onclick: this.sendHeadset.bind(this)
+                });
+            }
+        }, 
+        _addToggleToVBox: function () {
+            var oVBox = this.byId("myVBoxToggle");
+            if (oVBox) {
+                oVBox.addEventDelegate({
+                    onclick: this.onTogglePress.bind(this)
+                });
+            }
+        }, 
+        _closeToVBox: function () {
+            var oVBox = this.byId("closeVBox");
+            if (oVBox) {
+                oVBox.addEventDelegate({
+                    onclick: this.onClosePress.bind(this)
+                });
+            }
+        }, 
+        onTogglePress: function () { 
+            // Alterna el estado de reproducción
+            this._isPlaying = !this._isPlaying;  
+
+            // Obtiene el ícono dentro del VBox
+            var oIcon = this.byId("toggleIcon");
+
+            var oModel = this.getView().getModel("onContainer3");
+            // Cambia el ícono del botón según el estado
+            if (!this._isPlaying) {
+                oIcon.setSrc("sap-icon://media-play");  
+                oModel.setProperty("/visibilityHeadset", false); 
+            } else {
+                oIcon.setSrc("sap-icon://media-pause");
+                oModel.setProperty("/visibilityHeadset", true); 
+            }
+        },
+
+        onClosePress: function () { 
+            this._isPlaying = false;  
+            var oModel = this.getView().getModel("onContainer3");
+            oModel.setProperty("/visibility", false);
+            
         }
     });
 });
